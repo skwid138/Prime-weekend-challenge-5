6fd19a7ac9197148c5bee335cc0c26bc3348d7e8
Hunter Rancourt <skwid138@gmail.com>
2017-09-30 14:54:45 -0500

    moved current table code over to sales as well as rentals

--- public/scripts/controllers/sales.controller.js
+++ public/scripts/controllers/sales.controller.js
@@ -2,6 +2,12 @@ myApp.controller('SalesController', function (ListingService) {
     console.log('in SalesController');
     var vm = this;
 
+    // sets vm.rentals to the object self.rentals with a prop of list
+    vm.sales = ListingService.sales;
+
+    vm.limit = 10;
+    vm.page = 1;
+
     vm.showSales = function () {
         ListingService.getSales();
     }; // end showSales
